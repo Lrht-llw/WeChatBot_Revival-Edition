09c7fd3463329c842a5b907c1504bc456205874d
Lr灰弹 <2179725762@qq.com>
2026-01-18 04:32:07 +0800

    软件安装

---
+++ 软件/微信_4.1.6.bat
@@ -0,0 +1,44 @@
+@echo off
+set "url=https://dldir1v6.qq.com/weixin/Universal/Windows/WeChatWin_4.1.6.exe"
+
+:retry
+cls
+echo ========================================
+echo	微信 4.1.6 下载程序
+echo   请选择浏览器：
+echo    1. 默认浏览器
+echo    2. Chrome
+echo    3. Edge
+echo    4. Firefox
+echo 		--- Lr灰弹科技工作室
+echo ========================================
+
+echo.
+set /p choice="请输入选择 (1-4): "
+
+if "%choice%"=="1" (
+    start %url%
+    goto success
+)
+if "%choice%"=="2" (
+    start chrome %url%
+    goto success
+)
+if "%choice%"=="3" (
+    start msedge %url%
+    goto success
+)
+if "%choice%"=="4" (
+    start firefox %url%
+    goto success
+)
+
+echo.
+echo 输入错误！请按任意键重新选择...
+pause >nul
+goto retry
+
+:success
+echo 浏览器正在打开链接...
+timeout /t 2 >nul
+start %url%
